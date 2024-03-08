826e27207742f3b30dc106430da8fda21b99847e
kdn <89808297+kdnnn@users.noreply.github.com>
2024-03-08 20:10:13 +0800

    V0.2
    Minor UI Improvements
    - Added login menu

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+function popupOnClick() {
+  document.getElementById("login-menu").classList.toggle("login-popup");
+}
